17676842034701b0ccdde3aabf24809c220a9775
tianxing <jackcaotianxing@163.com>
2019-10-22 18:29:29 +0800

    1

--- src/containers/entry/index.jsx
+++ src/containers/entry/index.jsx
@@ -1,141 +1,220 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import { NavBar, Icon } from 'antd-mobile';
 import './less/index.less'
 import BScroll from 'better-scroll'
 class Entry extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //     Bscroll: '',
-  //   }
-  // }
-  // componentDidMount() {
-  //   const wrapper = document.querySelector('.entry-content')
-  //   const scroll = new BScroll(wrapper, {
-  //     scrollX: false,
-  //     click: true,
-  //     scrollY:true
-  //   })
-  //   this.setState({
-  //     Bscroll: scroll,
-  //   })
-  // }
+  constructor(props) {
+    super(props)
+  }
+  state = {
+    Bscroll: '',
+    navIndex: 0,
+    scrollY: 0,
+    tops: []
+  }
   componentDidMount() {
     const wrapper = this.refs.wrapper
-    //选中DOM中定义的 .wrapper 进行初始化
-    new BScroll(wrapper, {
-      click: true,  // better-scroll 默认会阻止浏览器的原生 click 事件
-      scrollY: true
+    const scroll = new BScroll(wrapper, {
+      scrollX: false,
+      click: true,
+      scrollY: true,
+      probeType: 1
+    })
+    this.setState({
+      Bscroll: scroll,
+    })
+    this.initTops()
+    console.log(this.state.tops)
+    //监听滚动
+    scroll.on('scroll', ({ x, y }) => {
+      this.scrollY = Math.abs(y)
+      console.log(this.scrollY)
+    })
+    scroll.on('scrollEnd', ({ x, y }) => {
+      this.scrollY = Math.abs(y)
     })
   }
-  render () {
+  //初始化高度数组
+  initTops = () => {
+    const tops = []
+    let top = 0
+    tops.push(top)
+    const list = this.refs.rightList.children
+    Array.prototype.slice.call(list).forEach(item => {
+      top += item.clientHeight
+      tops.push(top)
+    })
+    this.state.tops = tops
+  }
+  //点击切换导航
+  changeIndex = (index) => {
+    return () => {
+      this.setState({
+        navIndex: index
+      })
+      // console.log(index)
+      console.log(this.state.navIndex)
+    }
+  }
+  componentWillUpdate() {
+    console.log('hha1')
+    const { scrollY, tops } = this.state
+    const index = tops.findIndex((top, index) => scrollY >= top && scrollY < tops[index + 1])
+    console.log(index)
+    if(this.state.navIndex!==index){
+      this.setState({
+        navIndex:index
+      })
+    }
+    console.log(this.state.navIndex)
+  }
+  render() {
     return (
-    <div className="entry">
-      <NavBar
-        mode="light"
-        icon={<Icon type="left" />}
-        onLeftClick={() => console.log('onLeftClick')}
-        rightContent={[
-          <Icon key="0" type="search" style={{ marginRight: '16px'}} />
-        ]}
-      >分类
+      <div className="entry">
+        <NavBar
+          mode="light"
+          icon={<Icon type="left" />}
+          onLeftClick={() => console.log('onLeftClick')}
+          rightContent={[
+            <Icon key="0" type="search" style={{ marginRight: '16px' }} />
+          ]}
+        >分类
       </NavBar>
-      <div className="entry-content">
-        <div className="leftWrapper">
-          <ul className="entry-left-list">
-            <li className="current">新品</li>
+        <div className="entry-content">
+          <div className="leftWrapper">
+            <ul className="entry-left-list">
+              {['新品', '众筹', '手机'].map((item, index) => {
+                return (
+                  <li key={index} onClick={this.changeIndex(index)} className={`${this.state.navIndex === index ? 'current' : ''}`}>{item}</li>
+                )
+              })}
+              {/* <li className="current">新品</li>
             <li>众筹</li>
             <li>手机</li>
             <li>电视</li>
             <li>新品</li>
             <li>众筹</li>
             <li>手机</li>
-            <li>电视</li>
-          </ul>
-        </div>
-        <div className="rightWrapper" ref="wrapper">
-          <div className="entry-content-right">
-            <div className="topImg">
-              <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/88306f97bb4402773aed2a830e92c465.jpg?thumb=1&w=500&h=200" alt=""/>
-            </div>
-            <div className="rightContent">
-              <div className="title">手机</div>
-              <ul className="rightUl">
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-              </ul>
-            </div>
-            <div className="rightContent">
-              <div className="title">手机</div>
-              <ul className="rightUl">
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-                <li>
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt=""/>
-                  <p>小米9</p>
-                </li>
-              </ul>
+            <li>电视</li> */}
+            </ul>
+          </div>
+          <div className="rightWrapper" ref="wrapper">
+            <div className="entry-content-right" ref="rightList">
+              <div className="rightContent">
+                <div className="title">手机</div>
+                <ul className="rightUl">
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                </ul>
+              </div>
+              <div className="rightContent">
+                <div className="title">手机</div>
+                <ul className="rightUl">
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                </ul>
+              </div>
+              <div className="rightContent">
+                <div className="title">手机</div>
+                <ul className="rightUl">
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                  <li>
+                    <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/a78e73027b202757a933083ddd6ffbe0.png?thumb=1&w=120&h=120" alt="" />
+                    <p>小米9</p>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     )
   }
 }
